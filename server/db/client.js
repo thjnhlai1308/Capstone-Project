8d08c0e5080245bd1d0598df981b535f2ec4823c
Thinh Lai <thinhlai1308@gmail.com>
2025-07-21 11:31:28 -0500

    draft commit

--- server/db/client.js
+++ server/db/client.js
@@ -1,4 +1,4 @@
 const pg = require('pg');
-const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/your_db_name_here');
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/virtual_closet');
 
 module.exports = client
